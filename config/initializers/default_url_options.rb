bcffd42db7f6edef1bc1ac7282ac64554307c092
vinibispo <vini.bispo015@gmail.com>
2023-12-12 09:38:45 -0300

    fix: fix things for deploying

--- config/initializers/default_url_options.rb
+++ config/initializers/default_url_options.rb
@@ -5,4 +5,7 @@ when "development"
   when "test"
   default_url_options = { host: "localhost", port: Capybara.server_port }
   Rails.application.routes.default_url_options = default_url_options
+  when "production"
+  default_url_options = { host: ENV.fetch("HOST", "urlcrew.co") }
+  Rails.application.routes.default_url_options = default_url_options
 end
