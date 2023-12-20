6e026aa02ac43c0008d42e0ff6740c76212cc760
vinibispo <vini.bispo015@gmail.com>
2023-12-20 20:50:52 -0300

    feat: add profiler feature on sentry config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -8,6 +8,7 @@ when 'production'
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
     config.traces_sample_rate = 1.0
+    config.profiles_sample_rate = 1.0
     # or
     config.traces_sampler = lambda do |context|
       true
