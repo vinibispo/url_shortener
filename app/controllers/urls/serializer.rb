09d53fbf1903491a0229b56b02f00e10feed525c
vinibispo <vini.bispo015@gmail.com>
2023-12-22 17:39:15 -0300

    feat: prepare for using turbo

--- app/controllers/urls/serializer.rb
+++ app/controllers/urls/serializer.rb
@@ -7,6 +7,14 @@ Urls::Serializer = Data.define(:short, :original, :date) do
     date.strftime("%F")
   end
 
+  def model_name
+    OpenStruct.new(param_key: "link")
+  end
+
+  def to_key
+    [short]
+  end
+
   def short_url
     Rails.application.routes.url_helpers.short_url(short)
   end
