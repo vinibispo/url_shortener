aca6e22df55fd48a58786bb6929952d1023a28c4
vinibispo <vini.bispo015@gmail.com>
2024-01-17 22:45:27 -0300

    feat(seo): add metatags, and keywords

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+
+  before_action :set_meta
+
+  def set_meta
+    page_meta.tag :author, 'Vinícius Bispo'
+
+    page_meta.tag :copyright, 'UrlCrew Inc.'
+
+    page_meta.tag :og, {
+      title: 'UrlCrew',
+      type: 'website',
+      url: 'https://urlcrew.co',
+      description: page_meta.description
+    }
+  end
 end
