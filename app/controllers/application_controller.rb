caeb82c6b8071a83da21c6f1a18626711a22c98f
vinibispo <vini.bispo015@gmail.com>
2024-01-19 08:46:50 -0300

    feat(favicon): add favicon and minify javascript in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,16 @@ class ApplicationController < ActionController::Base
       title: 'UrlCrew',
       type: 'website',
       url: 'https://urlcrew.co',
-      description: page_meta.description
+      description: page_meta.description,
+      image: '/apple-touch-icon.png'
     }
+
+    page_meta.link :apple_touch_icon, sizes: '180x180', href: '/apple-touch-icon.png'
+
+    page_meta.link :icon, type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png'
+
+    page_meta.link :icon, type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png'
+
+    page_meta.link :manifest, href: '/site.webmanifest'
   end
 end
