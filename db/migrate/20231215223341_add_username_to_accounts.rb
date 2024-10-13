6a50b02559c515f36c6c781106b05d75376e9c76
vinibispo <vini.bispo015@gmail.com>
2024-10-13 14:17:48 -0300

    Remove unneeded migrations

--- db/migrate/20231215223341_add_username_to_accounts.rb
+++
@@ -1,5 +0,0 @@
-class AddUsernameToAccounts < ActiveRecord::Migration[7.1]
-  def change
-    add_column :accounts, :username, :string, null: false, default: ''
-  end
-end
--- db/migrate/20231215235221_add_account_id_to_urls.rb
+++
@@ -1,7 +0,0 @@
-class AddAccountIdToUrls < ActiveRecord::Migration[7.1]
-  def change
-    add_column :urls, :account_id, :integer
-
-    add_index :urls, :account_id
-  end
-end
